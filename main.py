efaee2203658bf04dcbc8c2a91e1035ac9626b6b
wasifnaseem <wkhsn2013@gmail.com>
2022-03-13 20:50:16 +0300

    debugging postgresql variable

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
